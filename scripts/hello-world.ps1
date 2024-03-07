dd4d58b63a083df9944086062819fc737763708d
Ciaran Hasson <c.hasson@kainos.com>
2024-03-07 15:25:33 +0000

    inital pipeline just to checkout out github
    inital pipeline just to checkout out github
    
    inital pipeline just to checkout out github
    
    inital pipeline just to checkout out github
    
    inital pipeline just to checkout out github
    
    inital pipeline just to checkout out github
    
    inital pipeline just to checkout out github
    
    inital pipeline just to checkout out github
    
    inital pipeline just to checkout out github
    
    inital pipeline just to checkout out github
    
    inital pipeline just to checkout out github
    
    inital pipeline just to checkout out github
    
    inital pipeline just to checkout out github
    
    inital pipeline just to checkout out github
    
    inital pipeline just to checkout out github
    
    inital pipeline just to checkout out github
    
    inital pipeline just to checkout out github
    
    inital pipeline just to checkout out github
    
    inital pipeline just to checkout out github
    
    inital pipeline just to checkout out github
    
    inital pipeline just to checkout out github
    
    inital pipeline just to checkout out github
    
    inital pipeline just to checkout out github
    
    inital pipeline just to checkout out github
    
    inital pipeline just to checkout out github
    
    inital pipeline just to checkout out github
    
    inital pipeline just to checkout out github
    
    inital pipeline just to checkout out github
    
    inital pipeline just to checkout out github
    
    inital pipeline just to checkout out github
    
    inital pipeline just to checkout out github
    
    inital pipeline just to checkout out github
    
    inital pipeline just to checkout out github
    
    inital pipeline just to checkout out github
    
    inital pipeline just to checkout out github
    
    inital pipeline just to checkout out github
    
    inital pipeline just to checkout out github
    
    inital pipeline just to checkout out github
    
    inital pipeline just to checkout out github
    
    inital pipeline just to checkout out github
    
    inital pipeline just to checkout out github
    
    inital pipeline just to checkout out github
    
    inital pipeline just to checkout out github
    
    inital pipeline just to checkout out github
    
    inital pipeline just to checkout out github
    
    inital pipeline just to checkout out github
    
    inital pipeline just to checkout out github
    
    inital pipeline just to checkout out github
    
    inital pipeline just to checkout out github
    
    inital pipeline just to checkout out github
    
    inital pipeline just to checkout out github
    
    inital pipeline just to checkout out github
    
    inital pipeline just to checkout out github
    
    inital pipeline just to checkout out github
    
    inital pipeline just to checkout out github
    
    inital pipeline just to checkout out github
    
    inital pipeline just to checkout out github
    
    inital pipeline just to checkout out github
    
    inital pipeline just to checkout out github
    
    inital pipeline just to checkout out github
    
    inital pipeline just to checkout out github

---
+++ scripts/hello-world.ps1
@@ -0,0 +1,10 @@
+[CmdletBinding()]
+param(
+    #the artifact to install.
+    [Parameter(Mandatory = $false)]
+    [string]$Tag = "001"
+)
+
+Set-Location .\stw-mapping-library
+
+mvn clean install -pl trade-mapping-to-ipaffs -am --settings /home/vsts/work/1/s/stw-ops/scripts/settings/settings.xml
