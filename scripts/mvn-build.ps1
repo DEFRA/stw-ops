275f9cdb885d5f79c6a1d5118f07a0e05d732181
Ciaran Hasson <c.hasson@kainos.com>
2024-03-12 10:05:57 +0000

    inital pipeline just to checkout out github

--- scripts/mvn-build.ps1
+++ scripts/mvn-build.ps1
@@ -5,8 +5,13 @@ param(
     [string]$Tag = "001"
 )
 
+#AGENT COMMANDS
+
 Set-Location .\stw-mapping-library
+mvn package -pl trade-mapping-to-ipaffs -am --settings /home/vsts/work/1/s/stw-ops/scripts/settings/settings.xml
 
-curl -O https://artifactoryv2.azure.defra.cloud/artifactory/PLT-SNAPSHOT/settings.xml
 
-mvn clean install -pl trade-mapping-to-ipaffs -am --settings /home/vsts/work/1/s/stw-ops/scripts/settings/settings.xml
+#LOCAL COMMANDS
+
+# Set-Location /Users/ciaranha/repos/DEFRA-STW/stw-mapping-library
+# mvn clean install -pl trade-mapping-to-ipaffs -am --settings /Users/ciaranha/repos/DEFRA-STW/stw-ops/scripts/settings/settings.xml
